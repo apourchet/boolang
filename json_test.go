032cc9a9d504f8b5ac8e2f19de69def832e32c12
Antoine Pourchet <antoine.pourchet@gmail.com>
2017-08-21 08:33:54 -0700

    Added pointer test to json

--- json_test.go
+++ json_test.go
@@ -21,6 +21,19 @@ func TestParseJson(t *testing.T) {
 	assert.Equal(t, 2, counter)
 }
 
+func TestParseJsonPtr(t *testing.T) {
+	var data struct {
+		AST *boolang.JsonAST `json:"ast"`
+	}
+	var content = []byte(`{"ast":"A==1 && B == 2"}`)
+	err := json.Unmarshal(content, &data)
+	assert.Nil(t, err)
+
+	counter := 0
+	data.AST.Walk(func(l *boolang.Leaf) { counter += 1 })
+	assert.Equal(t, 2, counter)
+}
+
 func TestParseJsonError(t *testing.T) {
 	var data struct {
 		AST boolang.JsonAST `json:"ast"`
